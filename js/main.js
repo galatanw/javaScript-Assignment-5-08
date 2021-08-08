580271d5397a785fe0d8ccedd969d3eb2725e2aa
galatanw <galatanw@icloud.com>
2021-08-08 13:57:55 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,270 @@
+// // answer 1
+
+// var ageMitzva=Number(prompt("what is your age"))
+// if(isNaN(ageMitzva)){
+//     for (let index = 0; index < 10; index++) {
+//      alert("insert numbers only!"); ageMitzva=Number(prompt("what is your age"))
+//      if(ageMitzva){break}}
+// }
+// var ageMitzva2=Number(prompt("what is your age"))
+// if(isNaN(ageMitzva2)){
+//     for (let index = 0; index < 10; index++) {
+//      alert("insert numbers only!"); ageMitzva2=Number(prompt("what is your age"))
+//      if(ageMitzva2){break}}
+// }
+// var sum=ageMitzva+ageMitzva2
+   
+// if(sum>=13){console.log("BAR-MITZVA")}
+// else if(sum==12){console.log("BAT-MITZVA")}
+// else{console.log("not old enough")}
+
+
+
+// // answer 2
+
+
+// var yesCounter=0
+// while(yesCounter<100){
+//     console.log("yes");
+//     yesCounter++
+// }
+
+
+
+
+// // answer 3
+
+
+// var whileSelect = Number(prompt("times printing 'no'"))
+// while(whileSelect>0){
+//     console.log("no")
+//     whileSelect--
+// }
+
+
+
+// // answer 4
+
+
+
+
+// var javaCounter=0
+// while(javaCounter<1000){
+//     console.log("javascript is not JAVA")
+//     javaCounter++
+// }
+
+
+
+
+// // anser 5
+
+
+// var userName = prompt("what's your name")
+// if(Number(userName)){
+//     for (let index = 0; index < 10; index++) {
+//      if(Number(userName)){
+//          alert("insert letters only!");userName=prompt("what is your name")}
+//          else{break}
+//     }
+// }
+// var nameCounter=Number(prompt("how many times should i repeat your name?"))
+// if(isNaN(nameCounter)){
+//     for (let index = 0; index < 10; index++) {
+//      if(Number(nameCounter)){break}
+//          else{alert("insert numbers only!");nameCounter=Number(prompt("how many times should i repeat your name?"))}
+//         }
+// }
+// while (nameCounter>0) {
+//     console.log(userName)
+//     nameCounter--
+// }
+
+
+
+
+
+// // answer 6
+
+
+
+
+// var usrNmeAns6= prompt("what is your name")       
+// var userAge=Number(prompt("what's your age?"))
+// if (isNaN(userAge)){
+//     for (let index = 0; index < 5; index++) {
+//         if(userAge){break}else{alert("insert your age in numbers");userAge=Number(prompt("what's your age?"))}
+// }}
+// var userMail = prompt("insert email, example:abc@abc.com")
+// var infoLoop = Number(prompt("how many times sould I print your info"))
+// for (let index = 0; index < infoLoop; index++) {
+//     console.log({usrNmeAns6,userAge,userMail})    
+// }
+
+
+
+
+
+
+// // answer 10
+
+
+
+// var bossCount,containI
+// bossCount=Number(prompt("times printing order"))
+// let bossName = prompt("type Gal atanw")
+// if (bossName=="Gal atanw"){for (var index = 0; index <bossCount; index++){console.log("hi boss Gal atanw")}}else{alert("no entry")}
+
+
+
+
+
+// // answer 11
+
+
+
+
+
+// var firstMultiply=Number(prompt("enter number"))
+// if(firstMultiply){}
+// else{for(var i=0;i<4;i++){if (firstMultiply){break}else{ alert("only numbers");firstMultiply=Number(prompt("enter number"))}}}
+// var secondeMultiply=Number(prompt("multiplier would be printed "+firstMultiply+" * X insert X"))
+// if(secondeMultiply){}
+// else{for(var i=0;i<4;i++){if (secondeMultiply){break}else{ alert("only numbers");secondeMultiply=Number(prompt("multiplier would be printed "+firstMultiply+" * X insert X"))}}}
+// myI=10
+// myJ=2
+// for(var i=0;i<firstMultiply;i++){
+//     for(var j=0;j<secondeMultiply;j++){
+//         console.log(myJ*myI)
+//     }
+// }
+
+
+
+
+
+// // anser 12
+// var myArray=[2,4,5,2]
+// console.log(myArray)
+
+
+
+
+
+// // answer 13
+// var myArraystr=["a","b","c","d","e","f"]
+// for(i=0;i<myArraystr.length;i++){
+//     console.log(myArraystr[i]);
+// }
+
+
+
+
+
+// // answer 14
+
+
+
+// var evenAraay=[12,13,14,15,16,17,19]
+// for(i=0;i<evenAraay.length;i++){
+//     evenAraay[i]
+//     if(evenAraay[i]%2==0){
+//         console.log(evenAraay[i])
+//     }
+// }
+
+
+
+// // answer 15
+
+
+// var userNameAraay=[];
+// for(i=0;i<5;i++){
+//     userNameAraay.push(prompt("insert name"))
+// }console.log(userNameAraay)
+
+
+
+// // answer16
+
+// var unevenAraay=[];
+// for(i=0;i<7;i++){
+//     unevenAraay.push(Number(prompt("enter varibale")))
+    // if(unevenArray.push){}else{for(var i=0;i<5;i++){if(unevenArray){break}else{alert("can contin only numbers");unevenAraay.push(Number(prompt("enter varibale")))}}}
+//     unevenAraay[i]
+//     if(unevenAraay[i]%2==1){
+//         console.log(unevenAraay[i])
+//     }
+// }
+
+
+// // answer 17
+// var RandomTypeLopp=["a",2,"s",4,"w",4]
+// for(i=0;i<RandomTypeLopp.length;i++){
+//     if(isNaN(RandomTypeLopp[i])){
+//         RandomTypeLopp[i]="not_number"
+//     }
+// }console.log(RandomTypeLopp)
+
+
+// // answer 18
+
+
+
+// var evenUserArray=[]
+// var evenCounter=0
+// var userArray;
+// while(evenCounter<11){
+// userArray=Number(prompt("insert array elements"))
+//     if(userArray){}else{for(i=0;i<5;i++){if(userArray){}else{alert("can insert only number to array");userArray=Number(prompt("insert array elements"))}}}
+//     if(userArray%2==0){evenUserArray.push(userArray); evenCounter++}
+// }console.log(evenUserArray);
+
+
+
+
+// // answer 19
+
+
+// var rectHigt = Number(prompt("please insert the rectangle height"))
+// var recWidh= Number(prompt("Please insert the reactangle width"))
+// console.log(recWidh*rectHigt)
+
+
+// answer 20
+
+
+// var userA,userB,userC
+// userA=Number(prompt("First insert"))
+// if(userA){}else{for(i=0;i<4;i++){if(userA){break}else{alert("Numbers only");userA=Number(prompt("First insert"))  }}}
+// userB=Number(prompt("Seconde insert"))
+// if(userB){}else{for(i=0;i<4;i++){if(userB){break}else{alert("Numbers only");userB=Number(prompt("Seconde insert"))  }}}
+// userC=Number(prompt("Third insert"))
+// if(userC){}else{for(i=0;i<4;i++){if(userC){break}else{alert("Numbers only");userC=Number(prompt("Third insert"))  }}}
+
+// if(userA>userB&&userA>userC){
+//     console.log("order top to bottom");
+//     console.log( {userA , userB , userC});
+// }else if(userB>userA&&userB>userC){
+//     console.log("no order");
+//     console.log({ userA , userB , userC});
+// }else{console.log({ userA , userB, userC})
+// console.log("order bottom to top")}
+
+
+// // answer 21
+
+
+// var userNumber=Number(prompt("enter num to seprate"))
+// var userNumberCollector=[]
+// var modNum;
+// while(userNumber!=0){
+//     modNum=parseInt(userNumber%10)
+//     userNumberCollector.push(modNum)
+//     userNumber=parseInt(userNumber/10)
+// }
+// var lenghtCollect=userNumberCollector.length
+// for(i=0;i<userNumberCollector.length;i++){
+//     console.log(userNumberCollector[lenghtCollect-1]);
+//     lenghtCollect--
+// }
